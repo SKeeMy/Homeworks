562a092f130c9d9922de3d74fad87ee41c281b49
Vyacheslav Kaplin <slavakaplin84@gmail.com>
2023-02-28 11:26:01 +1000

    Add task04

---
+++ task04.py
@@ -0,0 +1,16 @@
+def check_sum_of_four(a: list[int], b: list[int], c: list[int], d: list[int]) -> int:
+    total = {}
+    for i in a:
+        for j in b:
+            if i+j not in total:
+                total[i+j] = 1
+            else: 
+                total[i+j] += 1
+            totalizer = 0
+            for i in c:
+                for j in  d:
+                    if -1 * (i+j) in total:
+                        totalizer += total[-1*(i+j)]
+    return totalizer
+
+print(check_sum_of_four([1,2], [-2,1], [-1,2], [0, 2]))
